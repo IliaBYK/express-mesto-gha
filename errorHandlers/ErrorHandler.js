3e6c4021e9b57fe7818e7792680e71292d5981b6
Ilia Bykov <fagotist96@icloud.com>
2023-01-09 18:46:51 +0300

    added url validation, fixed errors messages

--- errorHandlers/ErrorHandler.js
+++ errorHandlers/ErrorHandler.js
@@ -7,10 +7,12 @@ import {
 import NotFoundError from '../errors/NotFoundError.js';
 
 export default function errorHandler(err, req, res, next) {
-  if (err instanceof BadRequestError || Error.CastError || Error.ValidationError) {
-    res.status(BAD_REQUEST_ERR_CODE).send({ message: err.message });
-  } else if (err instanceof NotFoundError) {
+  if (err instanceof NotFoundError) {
     res.status(NOT_FOUND_ERR_CODE).send({ message: err.message });
+  } else if (err instanceof Error.CastError) {
+    res.status(NOT_FOUND_ERR_CODE).send({ message: err.message });
+  } else if (err instanceof BadRequestError || Error.ValidationError) {
+    res.status(BAD_REQUEST_ERR_CODE).send({ message: err.message });
   } else {
     next(err);
   }
